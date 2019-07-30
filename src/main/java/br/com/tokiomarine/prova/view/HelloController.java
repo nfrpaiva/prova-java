a1d4cc08b4f66c1a0b2036a102b12d63fe264a82
Nilton Paiva <nilton.paiva@tokiomarine.com.br>
2019-07-30 20:56:00 +0000

    Init

--- src/main/java/br/com/tokiomarine/prova/view/HelloController.java
+++ src/main/java/br/com/tokiomarine/prova/view/HelloController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.tokiomarine.prova.domain.Contato;
@@ -16,24 +18,46 @@ import br.com.tokiomarine.prova.domain.Contato;
 @RequestMapping("/hello")
 public class HelloController {
 
-    private static Logger  logger = LoggerFactory.getLogger(HelloController.class);
+    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping
-    private String getHello (Model model){
+    private String getHello(Model model) {
 
-        List<Contato> contatos =  new ArrayList<>();
-
-        Contato c1 = new Contato(1l, "Pessoa 01", "+5511999999999");
-        Contato c2 = new Contato(2l, "Pessoa 02", "+5511988888888");
-
-        contatos.add(c1);
-        contatos.add(c2);
+        List<Contato> contatos = contatos();
 
         logger.info("Contatos: {}", contatos);
 
         model.addAttribute("contatos", contatos);
+        model.addAttribute("contato1", new Contato());
 
         return "hello";
     }
-    
+
+    private List<Contato> contatos() {
+        List<Contato> contatos = new ArrayList<>();
+        Contato c1 = new Contato(1l, "Pessoa 01", "+5511999999999");
+        Contato c2 = new Contato(2l, "Pessoa 02", "+5511988888888");
+        contatos.add(c1);
+        contatos.add(c2);
+        return contatos;
+    }
+
+    @PostMapping("contato")
+    public String inserirContato (@ModelAttribute("contato1") Contato contato, Model model){
+        logger.info("Vejamos se o contato chegou : {}", contato);
+
+        List<Contato> contatos = contatos();
+
+        logger.info("Contatos: {}", contatos);
+
+        contatos.add(contato);
+
+        model.addAttribute("contatos", contatos);
+        model.addAttribute("contato1", new Contato());
+
+        return "hello";
+
+    }
+
+
 }
